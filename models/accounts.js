ea4cdd4dc76ee70769593f52a4195f30c74048ba
giselle2323 <yakubuaminat94@gmail.com>
2019-12-11 14:12:42 +0100

    Made the foodlist numbered and grouped the list according to the food chosen

---
+++ models/accounts.js
@@ -0,0 +1,136 @@
+const dbo = require('../lib/db.js')
+    , Slack = require('./slack.js')
+    , { WebClient } = require('@slack/client')
+    ;
+
+
+exports.updatePoll = async (team, token, data) => {
+  try {
+    const options = [];
+    let i = 0;
+    for (const option of data.options) {
+      options.push({
+                    text: {
+                      type: "plain_text",
+                      text: option,
+                      emoji: true
+                    },
+                    value: `${i}`
+                  });
+      i++;
+    }
+
+    //console.log(options);return;
+    const status = await dbo.db().collection('polls').updateOne({
+      day: (new Date()).toDateString(),
+      team
+    }, {
+      $set: {
+        token,
+        question: data.question,
+        options: data.options,
+        email: data.email,
+        channel: data.channel,
+        closingTime: data.closingTime
+      }
+    }, {
+      upsert: true
+    });
+
+    const slackAPI = new WebClient(token);
+    const response = await slackAPI.chat.postMessage({
+          channel: data.channel
+          , blocks: [{
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: data.question
+            }
+          }, {
+            type: "actions",
+            elements: [
+              {
+                type: "static_select",
+                placeholder: {
+                  type: "plain_text",
+                  text: "Make your selection",
+                  emoji: true
+                },
+                options
+              }
+            ]
+          }]
+        });
+
+    return response;
+  }
+  catch(err) {
+    err = (err.message) ? err.message : err;
+    throw new Error(err);
+  }
+}
+
+exports.todaysPoll = async(id) => {
+  try {
+    return await dbo.db().collection('responses').find({
+      team: id,
+      day: (new Date()).toDateString()
+    }).toArray();
+  }
+  catch(err) {
+    err = (err.message) ? err.message : err;
+    throw new Error(err);
+  }
+}
+
+exports.log = async(body) => {
+
+  const user = body.user;
+  const choice = body.actions[0].selected_option.value;
+
+  try {
+    // Get option
+    const doc = await dbo.db().collection('polls').findOne({
+      team: body.team.id,
+      day: (new Date()).toDateString()
+    });
+
+    const slack = new WebClient(doc.token);
+    const getFoodCloseTime = doc.closingTime;
+    const foodBotCloseTime = new Date(getFoodCloseTime).getTime();
+
+    // Closed?
+    if (doc.closed ||foodBotCloseTime < new Date().getTime() ) {
+      return await slack.chat.postEphemeral({
+        user: body.user.id
+        , channel: body.channel.id
+        , text: "FoodBot closed. No food for you 😛"
+      });
+    } 
+    await slack.chat.postEphemeral({
+        user: body.user.id
+        , channel: body.channel.id
+        , text: "Your choice has been logged 💥"
+      });
+    const selection = doc.options[+choice];
+    const time = new Date().toLocaleTimeString();
+    
+    await dbo.db().collection('responses').updateOne({
+      day: (new Date().toDateString()),
+      'user.id': user.id,
+      team: body.team.id
+    }, {
+      $set: {
+        time,
+        selection,
+        user
+      }
+    }, {
+      upsert: 1
+    });//*/
+  }
+  catch(err) {
+    err = (err.message) ? err.message : err;
+    throw new Error(err);
+  }
+}
---
+++ routes/main.js
@@ -0,0 +1,121 @@
+const {render} = require('../lib/utils.js')
+
+    , SlackStrategy = require('@aoberoi/passport-slack').default.Strategy
+    , Slack = require('../models/slack.js')
+    , Accounts = require('../models/accounts.js')
+    , _ = require('lodash')
+    ;
+
+module.exports = (router, passport) => {
+
+  passport.use('slack', new SlackStrategy({
+    clientID: process.env.SLACK_CLIENT_ID,
+    clientSecret: process.env.SLACK_CLIENT_SECRET,
+    callbackURL: process.env.SLACK_CB,
+    skipUserProfile: true,
+    state: true
+  }, (token, scopes, team, { bot, incomingWebhook }, profile, done) => {
+    if (token && team)
+      return done(null, {
+          token: token
+          , team: team
+        });
+    else
+      return done('Error authenticating your Slack account.');
+  }));
+
+  // New signup
+  router.get('/auth', passport.authenticate('slack', {
+    scope: ['chat:write:bot', 'channels:read']
+  }));
+  router.get('/auth/cb', (req, res, next) => {
+    passport.authenticate('slack', (err, profile) => {
+      if (err || !profile.team || !profile.token || !profile.team.id) {
+        req.flash('error', 'There has been an authentication error.'+
+          ' Please try again later');
+
+        bugsnagClient.notify(err);
+        return res.redirect('/');
+      }
+
+      req.login(profile, err => {
+        if (err)
+          req.flash('error', 'There has been a login error.'+
+            ' Please try again later.');
+
+        //return res.redirect('/day/'+(new Date()).toISOString().substring(0, 10));
+        return res.redirect('/dashboard');
+      });
+    })(req, res, next);
+  });
+
+  router.get('/', async (req, res) => {
+    res.render('index', render(req));
+  });
+  router.post('/', async (req, res) => {
+    try {
+      res.end();
+
+      const body = JSON.parse(req.body.payload);
+      console.log(body);
+
+      if (!body.type)
+        return;
+
+      if (body.type != 'block_actions')
+        return;
+
+      await Accounts.log(body);
+    }
+    catch(e) {
+      console.log(e);
+      res.end();
+    }
+  })
+
+  router.get('/dashboard', async (req, res) => {
+    try {
+      const responses = await Accounts.todaysPoll(req.user.team.id);
+      const property = "selection";
+      if (responses && responses.length) {
+        const groupBy = key => responses =>
+        responses.reduce((accumulator, obj) => {
+          const value = obj[key];
+          accumulator[value] = (accumulator[value] || []).concat(obj);
+          return accumulator;
+        }, {});
+        const groupBySelection = groupBy('selection');
+        const obj = JSON.stringify(groupBySelection(responses));
+          return res.render('poll-stats', render(req, {
+            responses, obj
+          }));
+        const channels = await Slack.getChannels(req.user.token);
+        res.render('new-poll', render(req, {
+          channels
+        }));
+      }
+    }
+    catch(e) {
+      // ??
+      console.log(e);
+      res.redirect('/');
+    }
+  });
+  router.post('/dashboard', async (req, res) => {
+    try {
+      const response = await Accounts.updatePoll(req.user.team.id, req.user.token, req.body);
+      req.flash('info', 'Food options sent');
+      res.redirect('/dashboard');
+    }
+    catch(e) {
+      req.flash('error', e.message ? e.message : 'There has been an error. Try again later');
+      res.redirect('/dashboard');
+    }
+  });
+
+  // Logout
+  router.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+  });
+}
